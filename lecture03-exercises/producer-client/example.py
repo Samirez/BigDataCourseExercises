e7d977fae2021d04fb809a8db0f6352344584a4e
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2022-09-27 14:37:46 +0200

    kafka producer

--- lecture03-exercises/producer-client/example.py
+++ lecture03-exercises/producer-client/example.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from kafka import KafkaProducer
 # input
 string = str(input())
@@ -10,3 +11,17 @@ for msg in string:
     producer.send('book', string)
 
 producer.close()
+=======
+from operator import truediv
+from kafka import KafkaProducer
+producer = KafkaProducer(bootstrap_servers='kafka:9092')
+exit = False
+while not exit:
+    input = input()
+    if(input == "exit"):
+        exit == True
+        break
+    producer.send('foobar', )
+for _ in range(100):
+   
+>>>>>>> upstream/main
