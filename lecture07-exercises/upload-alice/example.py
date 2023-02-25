701cc7a1ae28b00dc1a8ccec6087a5a008e85e7d
ahnai17 <ahnai17@student.sdu.dk>
2023-02-25 13:06:42 +0100

    changes to last exercise

--- lecture07-exercises/upload-alice/example.py
+++ lecture07-exercises/upload-alice/example.py
@@ -1,34 +1,40 @@
 import pymysql
 
 # IP address of the MySQL database server, that is already started with datahub
-Host = "mysql"  
-# User name of the database server (root is required to enable manipulation of the database)
-User = "root"       
+
+
+Host = "mysql"
+# Username of the database server (root is required to enable manipulation of the database)
+User = "root"
 # Password for the database user
 Password = "datahub"
 # database you want to use
 database = "datahub"
-conn  = pymysql.connect(host=Host, user=User, password=Password, database=database)
-cur  = conn.cursor() 
+conn = pymysql.connect(host=Host, user=User, password=Password, database=database)
+cur = conn.cursor()
 
 # Your task is to replace the following code to ingest the individual words of Alice in Wonderland
 # First you need to read the file (the file is loaded into the root of the container at ./alice-in-wonderland.txt)
+file = './alice-in-wonderland.txt'
+'''hdfsclient = InsecureClient('http://localhost:9096', user='superuser')
+text = hdfsclient.read(file)'''
+words = []
+with open(file) as f:
+    text = f.read()
+    words = text.split()
+
 
 # Then you must split the content of the file into individual words
 
+
 # Then create a table called alice and insert the individual words as (id, word) into the table
 
-
-
-## Example of interacting with the database
-query = f"CREATE TABLE IF NOT EXISTS test (a int)"
+# Example of interacting with the database
+query = f"CREATE TABLE IF NOT EXISTS alice (id int NOT NULL, word varchar(20))"
 cur.execute(query)
 
-PRODUCT_ID = 1201
-price = 10000
-PRODUCT_TYPE = 'PRI'
-
-query = f"INSERT INTO test (a) VALUES ('{PRODUCT_ID}')"
+for j in range(len(words)):
+    query = f"INSERT INTO alice (id, word) VALUES ({j}, '{words[j]}')"
 cur.execute(query)
 print(f"{cur.rowcount} details inserted")
 conn.commit()
